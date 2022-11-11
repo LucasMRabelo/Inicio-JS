d086d6909ca0907018a24f56ce19626434c56d78
Lucas MRabelo <lucasmrabelo@outlook.com>
2022-11-11 20:52:16 -0300

    .

--- script.js
+++ script.js
@@ -4,3 +4,16 @@ let idade = 29
 let nome = "Lucas"
 console.log(nome)
 console.log(idade)
+let logado = true
+let lista = ["ovo", "leite", "polvilho", 20] /*array*/
+let nomeCompleto = { nome:"Lucas", idade: 29 } 
+
+console.log(typeof nome)
+
+//Exercício 
+let carro = "Ferrari"
+console.log(carro)
+let precobolo = 19.99
+console.log (precobolo)
+let cidade = "São Paulo"
+console.log (cidade)
