635c16bf8eb7d4dad2bfbdfa8945c3ee9b99173a
Lucas Rabelo <lucasmrabelo@outlook.com>
2022-11-28 20:05:21 -0300

    aula6

--- js/js02.js
+++ js/js02.js
@@ -1,2 +1,25 @@
 document.getElementById("texto").innerHTML="Meu preimeiro texto em <b>JS</b>";
 console.log("Oi isso é um console.log");
+var a = 1;
+var b = 5;
+var c = a + b;
+console.log(c);
+//js é case sensitive
+
+//Declaração de variaves
+var a,b,c;
+
+//atribuição de valores
+a = 2;
+b = 7;
+c = a + b;
+
+alert(c); 
+
+var nome, sobrenome, NomeCompleto;
+nome= "Lucas";
+sobrenome= "Rabelo";
+NomeCompleto= nome + " " + sobrenome;
+
+document.getElementById("texto").innerHTML=NomeCompleto;
+
