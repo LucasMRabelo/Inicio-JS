caae5f827524507e7dfddbdfe2721323a8c3e76e
Lucas Rabelo <lucasmrabelo@outlook.com>
2022-12-03 21:23:51 -0300

    Home

--- js/js02.js
+++ js/js02.js
@@ -23,3 +23,26 @@ NomeCompleto= nome + " " + sobrenome;
 
 document.getElementById("texto").innerHTML=NomeCompleto;
 
+//incremento e decremento ++ --
+
+var valor1, valor2, total;
+valor1 = 5;
+valor2 = 5;
+
+total = ++valor1;
+alert(total);
+
+document.getElementById("texto").innerHTML=valor1;
+
+//EVITAR A REPETIÇÃO DA VARIAVEL
+
+valor1 += valor2; //= valor1 = valor1 + valor2 
+
+document.getElementById("texto").innerHTML=valor1;
+
+//comparação
+
+total= (valor1 != valor2); //true or false (===)usado para valor e tipo
+// != diferente
+document.getElementById("texto").innerHTML=total;
+
